8bee99e23581673bc39448eb7b750dbb8c87d2f7
Hoang Dao <daohoang.hust@gmail.com>
2018-09-25 00:15:06 +0700

    2 bài tập tìm sum và max

---
+++ ex1sum.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <omp.h>
+#include <malloc.h>
+#include <time.h>
+int main()
+{
+    int *A, i, step = 0, sum = 0, id, n, Ns = 1, num_threads;
+    printf("Nhap n = ");
+    scanf("%d", &n);
+    A = (int *)malloc((n + 1) * sizeof(int));
+    for (i = 0; i < n; i++)
+        *(A + i) = i + 1;
+    *(A + n) = 0;
+    clock_t start = clock();
+    for (i = 0; i < n; i++)
+        sum += *(A + i);
+    clock_t end = clock();
+    printf("\n Running time normal: %f, Sum = %d", (double)(end - start) / CLOCKS_PER_SEC, sum);
+    i = 0;
+    clock_t start_prl = clock();
+    while (n > 1)
+    {
+        // printf("\nStep %d:\n", ++i);
+        Ns *= 2;
+        num_threads = n / 2;
+        n = (n + 1) / 2;
+        omp_set_num_threads(num_threads);
+#pragma omp parallel private(id)
+        {
+            id = omp_get_thread_num();
+            *(A + id * Ns) = *(A + id * Ns) + *(A + id * Ns + Ns / 2);
+            // printf(" from thread %d: A[%d] = %d\n", id, id * Ns, *(A + id * Ns));
+        }
+    }
+    clock_t end_prl = clock();
+    printf("\n Running time with parallel: %f, Sum = %d\n", (double)(end_prl - start_prl) / CLOCKS_PER_SEC, *A);
+    free(A);
+    return 0;
+}
